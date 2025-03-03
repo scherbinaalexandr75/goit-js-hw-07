76e79b4e5826a1d436e05066ca987fd2103656fd
Shcherbyna Oleksandr <Scherbinaalexandr75@gmail.com>
2025-03-03 18:01:49 +0200

    Summary

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+// З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
+
+// Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст
+// заголовка елемента(тегу < h2 >) і кількість елементів у категорії(усіх < li >, вкладених у нього).
+
+
+var content = [];
+document.querySelectorAll("h1,h3").forEach(function (item) {
+  content.push(item.textContent);
+});
+console.log(content.join());
