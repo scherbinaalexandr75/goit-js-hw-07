eb258228cd0704994bc3b74386dd4d8ab0fd0216
Shcherbyna Oleksandr <Scherbinaalexandr75@gmail.com>
2025-03-08 20:07:05 +0200

    Summary

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,17 @@
-// З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
+// З використанням властивостей і методів DOM-елементів,
+//  напиши скрипт, який:
 
-// Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
-// Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст
-// заголовка елемента(тегу < h2 >) і кількість елементів у категорії(усіх < li >, вкладених у нього).
+// Порахує й виведе в консоль кількість категорій
+// в ul#categories, тобто елементів li.item.
+// Для кожного елемента li.item у списку ul#categories
+// знайде й виведе в консоль текст
+// заголовка елемента(тегу < h2 >) і кількість елементів
+// у категорії(усіх < li >, вкладених у нього).
 
+const classItems = document.querySelectorAll(".item");
+console.log(`Number of categories: ${classItems.length}`);
 
-var content = [];
-document.querySelectorAll("h1,h3").forEach(function (item) {
-  content.push(item.textContent);
+classItems.forEach(function (items) {
+  console.log(`Category: ${items.firstElementChild.textContent}`);
+  console.log(`Elements: ${items.lastElementChild.children.length}`);
 });
-console.log(content.join());
--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,15 @@
+// Напиши скрипт для створення галереї зображень на основі масиву даних.
+// HTML містить список ul.gallery.
+
+// <ul class="gallery"></ul>
+
+// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>.
+// Ти можеш створити й додати HTML-елементи, використовуючи document.createElement()
+// і elem.append() або шаблонні рядки і elem.insertAdjacentHTML().
+
+// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами через CSS класи.
+
 const images = [
   {
     url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
@@ -22,5 +34,23 @@ const images = [
   {
     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
     alt: "Lighthouse Coast Sea",
-  }
+  },
 ];
+
+const classGallery = document.querySelector(".gallery");
+const galleryImage = images.map((imgItem) => {
+  const li = document.createElement("li");
+  li.classList.add("class-img");
+
+  const img = document.createElement("img");
+  img.src = imgItem.url;
+  img.alt = imgItem.alt;
+  img.width = 412;
+  img.height = 256;
+  classGallery.append(li);
+  li.append(img);
+  return img;
+});
+
+console.log(classGallery);
+console.log(galleryImage);
